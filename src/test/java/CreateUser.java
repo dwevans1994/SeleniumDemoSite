a4cb93631d06fecbd341cd28c8eaf113d4dd75a7
Barnaby Baker <barnaby.baker@qa.com>
2017-08-08 16:41:40 +0100

    push

--- src/test/java/CreateUser.java
+++ src/test/java/CreateUser.java
@@ -1,12 +1,18 @@
 
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
@@ -18,9 +24,18 @@ import static org.junit.Assert.assertEquals;
 
 public class CreateUser {
 
+    private ExtentReports report;
+    private ExtentTest test;
+    private String reportFilePath = "report.html";
+
     private WebDriver webDriver;
     LoginPage l = new LoginPage();
+    SnapShot snap = new SnapShot();
 
+    @BeforeClass
+    public static void init(){
+
+    }
 
     @Before
     public void bTest() {
@@ -30,15 +45,29 @@ public class CreateUser {
 
         l = PageFactory.initElements(webDriver, LoginPage.class);
 
-
+        report = new ExtentReports();
+        ExtentHtmlReporter extentHtmlReporter = new ExtentHtmlReporter(reportFilePath);
+        extentHtmlReporter.config().setReportName("ReportName");
+        extentHtmlReporter.config().setDocumentTitle("DocumentTitle");
+        report.attachReporter(extentHtmlReporter);
+        test = report.createTest("TestName");
     }
+
+
 
     @After
     public void aTest() {
+
         System.out.println("After");
         webDriver.quit();
 
-    }
+        webDriver.quit();
+        report.flush();
+
+        }
+
+
+
 
     @Test
     public void CreateAcc_Login() throws InterruptedException {
@@ -46,6 +75,12 @@ public class CreateUser {
         System.out.println("Begin Test");
         webDriver.navigate().to("http://thedemosite.co.uk/login.php");
 
+
+        try {
+            snap.take(webDriver, "image1");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         l.goRegistration();
         l.sleep(2);
         l.enterCredentials("dylan", "brudda");
@@ -61,8 +96,9 @@ public class CreateUser {
 
 
         TimeUnit.SECONDS.sleep(5);
-        webDriver.quit();
 
+        test.log(Status.INFO,"Info level");
+        test.fail("Failed");
     }
 
 
